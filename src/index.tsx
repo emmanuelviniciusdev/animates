4d559290f6671d99f16d5089f7e8023962684cc5
Emmanuel Vinicius <emmanuelviniciusdev@gmail.com>
2021-03-04 09:33:24 -0300

    Remove arquivos desnecessários

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import reportWebVitals from './reportWebVitals'
 import Routes from './routes'
