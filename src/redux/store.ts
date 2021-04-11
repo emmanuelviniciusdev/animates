ddb49d847d7bb8126ec4da9592bea30de1a764dc
Emmanuel Vinicius <emmanuelviniciusdev@gmail.com>
2021-04-11 14:53:54 -0300

    implementa redux-thunk

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,9 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import auth from '../redux/ducks/auth'
 
 const rootReducer = combineReducers({ auth })
 
 export type RootState = ReturnType<typeof rootReducer>
 
-export default createStore(rootReducer)
+export default createStore(rootReducer, applyMiddleware(thunk))
