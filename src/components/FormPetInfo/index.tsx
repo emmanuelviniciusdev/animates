ec808235e4a55386c11218d1f4b0863bbc78371d
Emmanuel Vinicius <emmanuelviniciusdev@gmail.com>
2021-04-25 10:44:55 -0300

    adiciona mascara na input date

--- src/components/FormPetInfo/index.tsx
+++ src/components/FormPetInfo/index.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import AppInput from '../AppInput'
 import { Form, Textarea, WrapperInputCheckbox } from './styles'
+import InputMask from 'inputmask'
 
 function FormPetInfo() {
+    const dateOfBirthInputRef = useRef<HTMLInputElement>(null)
+
+    useEffect(() => {
+        if (dateOfBirthInputRef.current)
+            InputMask('99/99/9999').mask(dateOfBirthInputRef.current)
+    }, [])
+
     return (
         <>
             <Form>
                 <AppInput placeholder="nome" aria-label="nome" />
                 <AppInput
+                    ref={dateOfBirthInputRef}
                     placeholder="data de nascimento (dia/mês/ano)"
                     aria-label="data de nascimento (dia/mês/ano)"
                 />
