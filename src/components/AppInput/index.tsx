f32e8d42cf4cac6aab19c172a6c9c30fa565edb0
Emmanuel Vinicius <emmanuelviniciusdev@gmail.com>
2021-04-25 10:33:30 -0300

    implementa forwardRef em AppInput

--- src/components/AppInput/index.tsx
+++ src/components/AppInput/index.tsx
@@ -1,21 +1,23 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import { Props } from './types'
 import { Input, Line, WrapperInput, WrapperInputAndIcon } from './styles'
 import { Icon } from '@iconify/react'
 
-function AppInput({ icon, ...rest }: Props) {
-    return (
-        <>
-            <WrapperInput className="wrapper-input">
-                <WrapperInputAndIcon>
-                    {icon && <Icon icon={icon} className="icon" />}
-                    <Input {...rest} />
-                </WrapperInputAndIcon>
+const AppInput = forwardRef<HTMLInputElement, Props>(
+    ({ icon, ...rest }: Props, ref) => {
+        return (
+            <>
+                <WrapperInput className="wrapper-input">
+                    <WrapperInputAndIcon>
+                        {icon && <Icon icon={icon} className="icon" />}
+                        <Input ref={ref} {...rest} />
+                    </WrapperInputAndIcon>
 
-                <Line />
-            </WrapperInput>
-        </>
-    )
-}
+                    <Line />
+                </WrapperInput>
+            </>
+        )
+    }
+)
 
 export default AppInput
