2ddd82e29cfa79ee940a090c30f028fd7b6cf9ca
Emmanuel Vinicius <emmanuelviniciusdev@gmail.com>
2021-05-04 10:56:50 -0300

    efeito de carregamento para MatchCard em andamento

--- src/components/MatchCard/index.tsx
+++ src/components/MatchCard/index.tsx
@@ -7,6 +7,7 @@ import {
     InformationSeparator,
     Picture,
     ReportButton,
+    SkeletonCard,
 } from './styles'
 import tobby from '../../assets/images/tobby.jpg'
 import { Icon } from '@iconify/react'
@@ -50,56 +51,91 @@ function MatchCard() {
         }
     }
 
+    /**
+     * debug
+     */
+    const [showSkeleton, setShowSkeleton] = useState(true)
+
     return (
         <>
             <ReactTooltip id="tooltip-match-card" effect="solid" />
 
-            <motion.div
-                drag="x"
-                dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
-                dragElastic={0.7}
-                whileDrag={{ rotate: cardWhileDragRotate, cursor: 'grabbing' }}
-                onDrag={(event, info) =>
-                    setCardWhileDragRotate(info.offset.x >= 0 ? 5 : -5)
-                }
-                onDragEnd={matchProcess}
-            >
-                <WrapperCard>
-                    <Card>
-                        <Picture src={tobby} alt="Foto de perfil do(a) Tobby" />
+            {showSkeleton && (
+                <SkeletonCard>
+                    <div className="picture" />
+                    <div className="info" />
+                    <div className="action">
+                        <div className="btn" />
+                        <div className="btn" />
+                    </div>
+                    <div className="report-btn" />
+                </SkeletonCard>
+            )}
 
-                        <Information>
-                            <span>Tobby</span>
-                            <InformationSeparator />
-                            <span>3 meses</span>
-                        </Information>
+            {!showSkeleton && (
+                <>
+                    <motion.div
+                        drag="x"
+                        dragConstraints={{
+                            left: 0,
+                            right: 0,
+                            top: 0,
+                            bottom: 0,
+                        }}
+                        dragElastic={0.7}
+                        whileDrag={{
+                            rotate: cardWhileDragRotate,
+                            cursor: 'grabbing',
+                        }}
+                        onDrag={(event, info) =>
+                            setCardWhileDragRotate(info.offset.x >= 0 ? 5 : -5)
+                        }
+                        onDragEnd={matchProcess}
+                    >
+                        <WrapperCard>
+                            <Card>
+                                <Picture
+                                    src={tobby}
+                                    alt="Foto de perfil do(a) Tobby"
+                                />
 
-                        <Actions>
-                            <CardButton
-                                aria-label="Não amar"
-                                data-tip="Não amar"
-                                data-for="tooltip-match-card"
-                                icon={xBold}
-                            />
-                            <CardButton
-                                aria-label="Amar"
-                                data-tip="Amar"
-                                data-for="tooltip-match-card"
-                                icon={heartFill}
-                                hasIconHeart={true}
-                            />
-                        </Actions>
+                                <Information>
+                                    <span>Tobby</span>
+                                    <InformationSeparator />
+                                    <span>3 meses</span>
+                                </Information>
 
-                        <ReportButton
-                            aria-label="Denunciar"
-                            data-for="tooltip-match-card"
-                            data-tip="Denunciar"
-                        >
-                            <Icon icon={warningDuotone} className="icon" />
-                        </ReportButton>
-                    </Card>
-                </WrapperCard>
-            </motion.div>
+                                <Actions>
+                                    <CardButton
+                                        aria-label="Não amar"
+                                        data-tip="Não amar"
+                                        data-for="tooltip-match-card"
+                                        icon={xBold}
+                                    />
+                                    <CardButton
+                                        aria-label="Amar"
+                                        data-tip="Amar"
+                                        data-for="tooltip-match-card"
+                                        icon={heartFill}
+                                        hasIconHeart={true}
+                                    />
+                                </Actions>
+
+                                <ReportButton
+                                    aria-label="Denunciar"
+                                    data-for="tooltip-match-card"
+                                    data-tip="Denunciar"
+                                >
+                                    <Icon
+                                        icon={warningDuotone}
+                                        className="icon"
+                                    />
+                                </ReportButton>
+                            </Card>
+                        </WrapperCard>
+                    </motion.div>
+                </>
+            )}
         </>
     )
 }
